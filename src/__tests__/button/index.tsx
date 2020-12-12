d0bfc706f9c3a2b7920b217c55b321f189d8d3c5
jaluik <cqzhengmouren@gmail.com>
2020-12-12 23:47:54 +0800

    feat: 增加简单的btn测试用例

--- src/__tests__/button/index.tsx
+++ src/__tests__/button/index.tsx
@@ -3,7 +3,23 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Button from '@components/button/button';
 
-it('should have class ', () => {
-  render(<Button type='primary'>hello</Button>);
-  expect(screen.getByText('hello')).toHaveClass('seraphine-btn-primary');
+const btn_prefix = 'seraphine-btn';
+
+describe('it should render right classNames', () => {
+  it('should have default class', () => {
+    render(<Button>default</Button>);
+    expect(screen.getByText('default')).toHaveClass(`${btn_prefix}`);
+  });
+  it('should have primary class', () => {
+    render(<Button type='primary'>primary</Button>);
+    expect(screen.getByText('primary')).toHaveClass(`${btn_prefix}-primary`);
+  });
+  it('should have warn class', () => {
+    render(<Button type='warn'>warn</Button>);
+    expect(screen.getByText('warn')).toHaveClass(`${btn_prefix}-warn`);
+  });
+  it('should have circle class', () => {
+    render(<Button shape='circle'>circle</Button>);
+    expect(screen.getByText('circle')).toHaveClass(`${btn_prefix}-circle`);
+  });
 });
